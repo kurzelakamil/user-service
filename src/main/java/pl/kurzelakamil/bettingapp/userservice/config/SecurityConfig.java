699f35db70577045f2c528925df33decfd9c5f1b
kkurzela <kamil.kurzela@accenture.com>
2019-11-27 19:51:17 +0100

    initial commit

---
+++ src/main/java/pl/kurzelakamil/bettingapp/userservice/config/SecurityConfig.java
@@ -0,0 +1,57 @@
+package pl.kurzelakamil.bettingapp.userservice.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+
+
+@Configuration
+@EnableResourceServer
+public class SecurityConfig extends ResourceServerConfigurerAdapter {
+
+    @Value("${oauth2.config.signingKey}")
+    private String signingKey;
+
+    @Override
+    public void configure(final HttpSecurity http) throws Exception {
+                http
+                        .authorizeRequests()
+                        .anyRequest()
+                        .authenticated();
+    }
+
+    @Override
+    public void configure(final ResourceServerSecurityConfigurer config) {
+        config.tokenServices(tokenServices());
+    }
+
+    @Bean
+    public TokenStore tokenStore() {
+        return new JwtTokenStore(accessTokenConverter());
+    }
+
+    @Bean
+    public JwtAccessTokenConverter accessTokenConverter() {
+        final JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+        converter.setSigningKey(signingKey);
+        return converter;
+    }
+
+    @Bean
+    @Primary
+    public DefaultTokenServices tokenServices() {
+        final DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+        defaultTokenServices.setTokenStore(tokenStore());
+        return defaultTokenServices;
+    }
+
+}
